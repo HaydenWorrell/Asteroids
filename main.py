4994f05607d0f189c145f16951de05da7c64bf2c
HaydenWorrell <hayden.drake.worrell@gmail.com>
2025-02-04 13:13:05 -0700

    B: added the player and rotation functionality

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
+    clock = pygame.time.Clock()
+    dt = clock.tick(60) / 1000
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,7 +20,11 @@ def main():
                 return
 
         screen.fill('#000000', rect=None, special_flags=0)
+        clock.tick(60)
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+    
 
 if __name__ == "__main__":
     main()
